f886e6c6839ac064cc64a940caf361437faf2220
Johan Lopez <johanslopezc@gmail.com>
2024-07-25 16:32:40 -0500

    Add name to events

--- models/event.js
+++ models/event.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
     event: {
         type: String,
         required: true
