47bedaa578de20f67347f8e3efe0a5c74d3c7608
Johan Lopez <johanslopezc@gmail.com>
2025-03-04 08:39:04 -0500

    ARL with date now

--- models/arlModel.js
+++ models/arlModel.js
@@ -1,70 +1,74 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const arlSchema = new Schema({
-    arlName: {
-        type: String,
-        required: true
-    },
-    documentType: {
-        type: String,
-        required: true
-    },
-    cc: {
-        type: Number,
-        required: true
-    },
-    firstName: {
-        type: String,
-        required: true
-    },
-    secondName: {
-        type: String,
-        required: false
-    },
-    firstSurname: {
-        type: String,
-        required: true
-    },
-    secondSurname: {
-        type: String,
-        required: false
-    },
-    birthday: {
-        type: Date,
-        required: true
-    },
-    sex: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    address: {
-        type: String,
-        required: true
-    },
-    cellphone: {
-        type: Number,
-        required: true
-    },
-    eps: {
-        type: String,
-        required: true
-    },
-    afp: {
-        type: String,
-        required: true
-    },
-    city: {
-        type: String,
-        required: true
-    }
-})
+  arlName: {
+    type: String,
+    required: true,
+  },
+  documentType: {
+    type: String,
+    required: true,
+  },
+  cc: {
+    type: Number,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  secondName: {
+    type: String,
+    required: false,
+  },
+  firstSurname: {
+    type: String,
+    required: true,
+  },
+  secondSurname: {
+    type: String,
+    required: false,
+  },
+  birthday: {
+    type: Date,
+    required: true,
+  },
+  sex: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  cellphone: {
+    type: Number,
+    required: true,
+  },
+  eps: {
+    type: String,
+    required: true,
+  },
+  afp: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const Arl = mongoose.model('Arl', arlSchema);
+const Arl = mongoose.model("Arl", arlSchema);
 
 module.exports = Arl;
